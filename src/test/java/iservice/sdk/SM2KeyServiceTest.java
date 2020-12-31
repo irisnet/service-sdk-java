3c5dc40862ee22075d1fc3c49ef380d8bad9183d
seekelvis <358191817@qq.com>
2020-12-31 15:59:13 +0800

    delete useless comment in Test File.

--- src/test/java/iservice/sdk/SM2KeyServiceTest.java
+++ src/test/java/iservice/sdk/SM2KeyServiceTest.java
@@ -5,16 +5,13 @@ import iservice.sdk.entity.Mnemonic;
 import iservice.sdk.entity.SignAlgo;
 import iservice.sdk.entity.options.ServiceClientOptions;
 import iservice.sdk.module.IKeyService;
-import org.bouncycastle.util.encoders.Hex;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
-import org.web3j.crypto.Hash;
 
 import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -94,33 +91,6 @@ public class SM2KeyServiceTest {
         String address = keyService.importFromKeystore("test1", "12345678", "12345678",(String) paramMap.get(KEYS.FILEPATH));
         assertNotNull(address);
         assertEquals("Wrong Address", paramMap.get(KEYS.ADDRESS), address);
-    }
-
-//    @Test
-    public void test7() throws Exception {
-//        String address = keyService.importFromKeystore("test1", "12345678", "12345678",(String) paramMap.get(KEYS.FILEPATH));
-//        assertNotNull(address);
-//        assertEquals("Wrong Address", paramMap.get(KEYS.ADDRESS), address);
-//        String algoName = "tendermint/PrivKeySecp256k1";
-        String algoName = "cosmos/PrivKeySm2";
-//        String algoName = "sm2";
-        byte[] hash = Hash.sha256(algoName.getBytes(StandardCharsets.UTF_8));
-        System.out.println(Hex.toHexString(hash));
-        int ptr = 0;
-        while (hash[ptr] == 0) ptr++;
-        ptr += 3;
-        while (hash[ptr] == 0) ptr++;
-        byte[] prefix = new byte[5];
-        System.arraycopy(hash, ptr, prefix, 0, 4);
-        prefix[4] = 32;
-
-        System.out.println(Hex.toHexString(prefix));
-
-//        hash=trimLeadingZero(hash);
-//        hash = hash[3:]
-//        hash=trimLeadingZero(hash);
-//        prefix= hash[0:4]
-
     }
 
 }
