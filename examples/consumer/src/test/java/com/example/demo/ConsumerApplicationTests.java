1893f94c0772360d3935fd33d6d8c39f429d1ad9
顾 天乐 <tianle@bianjie.ai>
2021-01-08 13:49:06 +0800

    add examples

---
+++ examples/consumer/src/test/java/com/example/demo/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package examples.consumer.src.test.java.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
