d1f40024d2bbad5ae642bb880937ba810a3b13ed
chengwenxi <vincent.ch.cn@gmail.com>
2021-02-23 15:04:33 +0800

    add unit test workflow and fix test

--- src/test/java/iservice/sdk/KeyServiceTest.java
+++ src/test/java/iservice/sdk/KeyServiceTest.java
@@ -8,10 +8,9 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,9 +24,16 @@ import static org.junit.Assert.assertTrue;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class KeyServiceTest {
 
-    private static IKeyService keyService = ServiceClientFactory.getInstance().setOptions(new ServiceClientOptions()).getClient().getKeyService();
+    private ServiceClientOptions options = new ServiceClientOptions();
+    private static IKeyService keyService;
     private static final String HRP = "iaa";
     private static Map<KEYS, Object> paramMap = new HashMap<>();
+
+    public KeyServiceTest() throws URISyntaxException {
+        options.setGrpcURI((new URI("http://localhost:9090")));
+        keyService = ServiceClientFactory.getInstance().setOptions(options).getClient().getKeyService();
+    }
+
     private enum KEYS {
         ADDRESS,
         MNEMONIC,
@@ -36,7 +42,6 @@ public class KeyServiceTest {
 
     @Test
     public void test1AddKey() throws Exception {
-
         Mnemonic mnemonic = keyService.addKey("test", "123456");
         assertNotNull(mnemonic.getAddress());
         assertNotNull(mnemonic.getMnemonic());
@@ -75,10 +80,7 @@ public class KeyServiceTest {
 
     @Test
     public void test6ImporttKey() throws Exception {
-        BufferedReader bfr = Files.newBufferedReader(Paths.get((String) paramMap.get(KEYS.FILEPATH)));
-        String keystore = bfr.readLine();
-        bfr.close();
-        String address = keyService.importFromKeystore("test1", "123456", "123456", keystore);
+        String address = keyService.importFromKeystore("test1", "123456", "123456", (String) paramMap.get(KEYS.FILEPATH));
         assertNotNull(address);
         assertEquals("Wrong Address", paramMap.get(KEYS.ADDRESS), address);
     }
